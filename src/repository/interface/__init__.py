c7bc402e4727d75c5fea61c824a2a0a4bfd00bfd
Lee Hyun Jun <bhk0827@gmail.com>
2025-03-03 11:22:36 +0900

    feat : easy import for repository interface

---
+++ src/repository/interface/__init__.py
@@ -0,0 +1,3 @@
+from repository.interface.user_repository import IUserRepository
+from repository.interface.project_repository import IProjectRepository, IProjectMemberRepository
+from repository.interface.tenant_repository import ITenantRepository
