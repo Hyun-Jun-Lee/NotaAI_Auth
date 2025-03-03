736507105ff556776d79332385e02ce6e08ddbbd
Lee Hyun Jun <bhk0827@gmail.com>
2025-03-03 21:08:07 +0900

    fix : role exception message

--- src/exception/domain/role_exception.py
+++ src/exception/domain/role_exception.py
@@ -1,8 +1,8 @@
 
 class InvalidRoleException(Exception):
     """유효하지 않은 역할이 있는 경우 발생하는 예외"""
-    def __init__(self, message: str):
-        super().__init__(message)
+    def __init__(self, role: str = None):
+        super().__init__(f"Invalid role: {role}")
 
 class UnauthorizedAccessException(Exception):
     """사용자가 권한이 없는 경우 발생하는 예외"""
