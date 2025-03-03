6b2ed78d0a4d32557beba1e11c33efd13ecf9f6b
Lee Hyun Jun <bhk0827@gmail.com>
2025-03-03 21:54:55 +0900

    feat : easy import & register router

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -1,3 +1,4 @@
 from api.project_api import router as project_router
 from api.tenent_api import router as tenent_router
 from api.user_api import router as user_router
+from api.auth_api import router as auth_router
