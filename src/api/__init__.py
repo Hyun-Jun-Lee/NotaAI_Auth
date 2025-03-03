bb1117ed709853671ec81190576744159b1dcec3
Lee Hyun Jun <bhk0827@gmail.com>
2025-03-03 21:32:21 +0900

    feat : init api

---
+++ src/api/__init__.py
@@ -0,0 +1,3 @@
+from api.project_api import router as project_router
+from api.tenent_api import router as tenent_router
+from api.user_api import router as user_router
---
+++ src/main.py
@@ -0,0 +1,10 @@
+import os
+
+from fastapi import FastAPI, Request
+from api import project_router, user_router, tenent_router
+
+app = FastAPI(docs_url="/api/docs", openapi_url="/api/openapi.json")
+
+app.include_router(user_router, prefix="/api")
+app.include_router(project_router, prefix="/api")
+app.include_router(tenent_router, prefix="/api")
